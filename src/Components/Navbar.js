649b384bbc5323605b0f9f314e08cb555bd8245a
randhir669RKY <117824315+randhir669RKY@users.noreply.github.com>
2023-04-04 13:44:33 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,23 +14,14 @@ export default function Navbar(prop) {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Button className="nav-link btn btn-dark navbar-brand"  href = "/TextEditor" style={{color:'white'}}>Text Editor</Button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Button className="nav-link btn btn-dark" href = "#" >Home <span className="sr-only">(current)</span></Button>
-            </li>
-            <li className="nav-item active">
-              <Button className="nav-link btn btn-dark" href = "#" >About<span className="sr-only">(current)</span></Button>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            
-            <button className="btn btn-outline-success my-2 my-sm-0" onClick={logout}>Logout</button>
-          </form>
-          </div>
-          </nav>
-
-        </div>
+      
+          <Button className="nav-link btn btn-dark navbar-brand" href="/TextEditor" style={{ color: 'white' }}>Text Editor</Button>
+          <Button className="nav-link btn btn-dark" href="#" >Home <span className="sr-only">(current)</span></Button>
+          <Button className="nav-link btn btn-dark" href="#" >About<span className="sr-only">(current)</span></Button>
+            <form className="d-flex ml-auto">
+            <Button className="btn btn-outline-success my-2 my-sm-0" onClick={logout}>Logout</Button>
+            </form>
+      </nav>
+    </div>
         )
 }
