d687dd6b19b17c6654a460e741fd9cd6e8f6459e
Kumar <randhir.d.kumar@capgemini.com>
2023-08-14 14:04:01 +0530

    footer ,nav and starred added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import styles from './Footer.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressCard} from '@fortawesome/free-solid-svg-icons';
-
 
 export default function Footer() {
   return (
